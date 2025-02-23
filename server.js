d68c599d9f5f8c560271dfe27a110c8bd4cc4943
Fahmin Guliyev <baku.fehmin@gmail.com>
2025-02-23 19:20:32 +0100

    added authorization header to protect middleware

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Enable CORS
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 // Set application routes
 routeConf(express, app);
--- src/middleware/protect.js
+++ src/middleware/protect.js
@@ -3,7 +3,15 @@ const jwt = require('jsonwebtoken');
 
 exports.protect = (permissions) =>
   asyncHandler(async (req, res, next) => {
-    const token = req.cookies.token;
+    // Try to get token from cookies or from Authorization header
+    let token = req.cookies.token;
+    if (
+      !token &&
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer ')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
 
     if (!token) {
       return res.status(401).json({
