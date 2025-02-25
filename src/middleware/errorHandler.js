352c6f728f1f6831f22157dd93b32c7575f8df85
Fahmin Guliyev <baku.fehmin@gmail.com>
2025-02-25 16:46:18 +0100

    errorHandler fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -24,7 +24,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Internal Server Error',
+    message: error.message || 'Internal Server Error',
   });
 };
 
