ce4dd882c17f9a96f3f03a0e47c4eafda9b35a83
Fahmin Guliyev <baku.fehmin@gmail.com>
2025-03-06 18:07:04 +0100

    typo fix in routeConf

--- src/config/RouteConf.js
+++ src/config/RouteConf.js
@@ -2,7 +2,7 @@ const routerConf = (express, app) => {
   app.use('/auth', require('../routes/auth'));
   app.use('/profile', require('../routes/profile'));
   app.use('/save', require('../routes/save'));
-  app.use('identification', require('../routes/identification'));
+  app.use('/identification', require('../routes/identification'));
   app.use('/card', require('../routes/card'));
 };
 
